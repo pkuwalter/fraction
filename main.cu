93d0a3e48e8fb4fc19a66d1fa85475ec244cb22f
pkuwalter <pkuwalter@qq.com>
2018-09-03 16:33:04 +0800

    frac

--- main.cu
+++ main.cu
@@ -209,7 +209,7 @@ int main(int argc, char ** argv)
 
     std::cout << "frac" << "\t" << "Iter" << "\t" << "Trav" << "\t" << "isColored" << "\t"<< "Total" << "\t" << "isRight" <<endl;
     
-    for (fraction = 0; fraction =< 1; fraction+=0.05)
+    for (fraction = 0; fraction <= 1; fraction+=0.05)
     {      
       test(filename.c_str(), start_index, max_iters, fraction);
     }   
